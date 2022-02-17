334214b87592817c8a000638175e1901ed829bb2
Kadir Osman Üst <kadirosmanust@happbros.com>
2022-02-17 20:52:01 +0300

    Add project files.

---
+++ KosuMesafesiOlcer/Program.cs
@@ -0,0 +1,94 @@
+﻿var flag = true;
+
+while (flag)
+{
+    
+    Console.WriteLine("--Merhabalar--");
+    Console.WriteLine();
+    var adimUzunlugu = GetIntValue("Ortalama bir adımınız kaç santimetre ? : ");
+    var adimSayisi = GetIntValue("Bir dakikada kaç adım atıyorsunuz ? : ");
+    var sure = GetTimeSpan();
+    Console.Clear();
+    var sonuc = Calculate.CalculateValue(adimSayisi, adimUzunlugu, sure);
+    Console.WriteLine($"Adım Uzunluğu = {adimUzunlugu}\nGeçen Zaman = {sure.ToString()}\nBir dakikada atılan adım sayısı = {adimSayisi}\nToplam atılan adım sayısı = {adimSayisi * sure.TotalMinutes}\n\n Toplam koşu mesafesi = {sonuc} Metre");
+
+    flag = WannaGoAgain();
+}
+double GetIntValue(string caption)
+{
+    var flag = true;
+
+    while (flag)
+    {
+        try
+        {
+            Console.Write(caption);
+            var value = Convert.ToDouble(Console.ReadLine());
+
+            return value;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Girmiş olduğunuz değer bir tam sayı değildir.");
+            continue;
+        }
+    }
+    return 0;
+}
+TimeSpan GetTimeSpan()
+{
+    var flag = true;
+
+    while (flag)
+    {
+        try
+        {
+            Console.WriteLine("Koşu sürenizi giriniz(Saat ve dakika olarak) ");
+            Console.WriteLine("");
+            Console.Write("Saat: ");
+            var kosuSuresiSaat = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Dakika: ");
+            var kosuSuresiDakika = Convert.ToInt32(Console.ReadLine());
+
+            var saat = new TimeSpan(kosuSuresiSaat, kosuSuresiDakika, 0);
+            return saat;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Girmiş olduğunuz değer bir tam sayı değildir.");
+            continue;
+        }
+    }
+    return default;
+}
+bool WannaGoAgain()
+{
+    while (true)
+    {
+        Console.WriteLine("Tekrar denemek ister misiniz?(e,evet)(k,kapat)");
+        var cevap = Console.ReadLine().ToUpper();
+        if (cevap == "E" || cevap == "EVET")
+        {
+            Console.Clear();
+            return true;
+        }
+        else if (cevap == "KAPAT" || cevap == "K")
+        {
+            Console.WriteLine("Görüşmek üzere...");
+            return false;
+        }
+        else
+        {
+            Console.WriteLine("Geçersiz cevap");
+        }
+    }
+}
+public static class Calculate
+{
+    public static double CalculateValue(double adimSayisi, double adimUzunlugu, TimeSpan sure)
+    {
+        double sonuc = adimSayisi * adimUzunlugu * sure.TotalMinutes * 0.01;
+
+        return sonuc;
+    }
+}
---
+++ UnitTestForKosuMesafesi/UnitTest1.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using System;
+
+namespace UnitTestForKosuMesafesi
+{
+    [TestFixture]
+    public class Tests
+    {
+    
+        [TestCase(32.2, 23, 30,222.18)]
+        [TestCase(31.2, 45, 434,6093.36)]
+        [TestCase(34.3, 23, 43,339.227)]
+        public void Calc(double adimSayisi, double adimUzunlugu, int sure, double expected)
+        {
+            var time = new TimeSpan(0, sure, 0);
+           var sonuc = Calculate.CalculateValue(adimSayisi,adimUzunlugu,time);
+
+            Assert.AreEqual(sonuc, expected);
+          
+        }
+    }
+}
